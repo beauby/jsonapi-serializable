e763161b3e2f0390afd033b867dc122559dea9b0
Lucas Hosseini <lucas.hosseini@gmail.com>
2016-10-23 22:28:23 +0200

    Extract Model DSL.

--- lib/jsonapi/serializable/model.rb
+++ lib/jsonapi/serializable/model.rb
@@ -1,67 +1,21 @@
+require 'jsonapi/serializable/model_dsl'
 require 'jsonapi/serializable/resource'
 
 module JSONAPI
   module Serializable
     class Model < Resource
+      include ModelDSL
+
       class << self
         attr_accessor :api_version_val
       end
 
+      def self.inherited(klass)
+        super
+        klass.api_version_val = api_version_val
+      end
+
       id { @model.public_send(:id).to_s }
-
-      def self.api_version(value)
-        self.api_version_val = value
-      end
-
-      def self.type(value = nil)
-        value ||= name
-        super(value)
-      end
-
-      def self.attribute(attr, &block)
-        block ||= proc { @model.public_send(attr) }
-        super(attr, &block)
-      end
-
-      def self.has_many(rel, resource_klass = nil, &block)
-        rel_block = proc do
-          if resource_klass
-            data do
-              @model.public_send(rel).map do |related|
-                resource_klass ||= resource_klass_for(related.class)
-                resource_klass.new(model: related)
-              end
-            end
-          end
-          instance_eval(&block) unless block.nil?
-        end
-        relationship(rel, &rel_block)
-      end
-
-      def self.has_one(rel, resource_klass = nil, &block)
-        rel_block = proc do
-          if resource_klass
-            data do
-              related = @model.public_send(rel)
-              resource_klass ||= resource_klass_for(related.class)
-              resource_klass.new(model: related)
-            end
-          end
-          instance_eval(&block) unless block.nil?
-        end
-        relationship(rel, &rel_block)
-      end
-
-      def nil?
-        @model.nil?
-      end
-
-      def as_jsonapi(params = {})
-        return nil if nil?
-        super(params)
-      end
-
-      private
 
       def resource_klass_for(model_klass)
         names = model_klass.name.split('::'.freeze)
@@ -74,6 +28,15 @@ module JSONAPI
                      .join('::'.freeze)
 
         Object.const_get(klass_name)
+      end
+
+      def nil?
+        @model.nil?
+      end
+
+      def as_jsonapi(params = {})
+        return nil if nil?
+        super(params)
       end
     end
   end
